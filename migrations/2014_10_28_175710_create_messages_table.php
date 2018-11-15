76f96712e1f7d32e330d280a6b4531223c57c548
Filip Petersson <fpetersson96@gmail.com>
2018-11-15 19:51:02 +0100

    Small migration fix

--- migrations/2014_10_28_175710_create_messages_table.php
+++ migrations/2014_10_28_175710_create_messages_table.php
@@ -16,7 +16,7 @@ class CreateMessagesTable extends Migration
     {
         Schema::create(Models::table('messages'), function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('thread_id')->unsigned();
+            $table->integer('conversation_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->text('body');
             $table->timestamps();
